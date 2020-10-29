5f31a0929f44e2e4e4e556375fcd282b5b14ff98
Sebastian Heyden <379651+czarly@users.noreply.github.com>
2020-10-29 21:00:48 +0400

    fix

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ git pull
 
 echo "[network] 
 public_address='$(dig @ns1-1.akamaitech.net ANY whoami.akamai.net +short):34553'" | crudini --merge etc/casper/config.toml
-#echo "[node] 
-#trusted_hash=$(curl -s 54.177.84.9:7777/status | jq .genesis_root_hash)" | crudini --merge etc/casper/config.toml
+echo "[node] 
+trusted_hash=$(curl -s 54.177.84.9:7777/status | jq .last_added_block_info)" | crudini --merge etc/casper/config.toml
 
 docker-compose up -d --build --force-recreate
