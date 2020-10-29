3f2f6e59b89bada8185918285ff456852fdabad6
Sebastian Heyden <379651+czarly@users.noreply.github.com>
2020-10-29 18:57:52 +0400

    fix

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 apt update -y
 apt install dnsutils
 
-echo "public_address = '$(dig @ns1-1.akamaitech.net ANY whoami.akamai.net +short):34553'" > etc/casper/config.toml
+echo "public_address = '$(dig @ns1-1.akamaitech.net ANY whoami.akamai.net +short):34553'" >> etc/casper/config.toml
