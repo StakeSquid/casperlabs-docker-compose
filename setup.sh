f49f56f9e10952cbe50b09f321e76cb61a5aa69c
Sebastian Heyden <379651+czarly@users.noreply.github.com>
2020-10-29 18:55:55 +0400

    update ip automatically

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+apt update -y
+apt install dnsutils
 
-echo "public_address = '135.181.73.218:34553'" > etc/casper/config.toml
+echo "public_address = '$(dig @ns1-1.akamaitech.net ANY whoami.akamai.net +short):34553'" > etc/casper/config.toml
