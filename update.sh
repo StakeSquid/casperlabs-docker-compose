8054b687316d57228ed2af1f734a62d089cc2918
Sebastian Heyden <379651+czarly@users.noreply.github.com>
2020-10-29 19:59:17 +0400

    remove some files

--- update.sh
+++ update.sh
@@ -8,4 +8,4 @@ public_address='$(dig @ns1-1.akamaitech.net ANY whoami.akamai.net +short):34553'
 #echo "[node] 
 #trusted_hash=$(curl -s 54.177.84.9:7777/status | jq .genesis_root_hash)" | crudini --merge etc/casper/config.toml
 
-docker-compose up --build --force-recreate
+docker-compose up -d --build --force-recreate
