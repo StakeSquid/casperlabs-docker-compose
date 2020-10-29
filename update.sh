b3ace1058804ffd2e9d8bc68c907e936c78b73ad
Sebastian Heyden <379651+czarly@users.noreply.github.com>
2020-10-29 19:49:24 +0400

    fix

--- update.sh
+++ update.sh
@@ -4,8 +4,9 @@ git checkout etc/casper/config.toml
 git pull
 
 echo "
-[agent]
+[network]
 public_address='$(dig @ns1-1.akamaitech.net ANY whoami.akamai.net +short):34553'
+[node]
 trusted_hash=$(curl -s 54.177.84.9:7777/status | jq .genesis_root_hash)" |
 
 crudini --merge etc/casper/config.toml
