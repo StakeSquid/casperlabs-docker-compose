f782a420ffc2f3233c9206817929af90097d40a4
Sebastian Heyden <379651+czarly@users.noreply.github.com>
2020-10-29 19:08:21 +0400

    fixes

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git pull
 git checkout etc/casper/config.toml
+git pull
 
 printf "\n\npublic_address = '$(dig @ns1-1.akamaitech.net ANY whoami.akamai.net +short):34553'" >> etc/casper/config.toml
 
