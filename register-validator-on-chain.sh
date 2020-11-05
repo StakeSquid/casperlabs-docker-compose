dc9614a7880e4fa7dfff6042c208d9514fec4b39
Trader_Payne <baboialex95@gmail.com>
2020-11-06 00:07:37 +0200

    Fixed reg script

--- register-validator-on-chain.sh
+++ register-validator-on-chain.sh
@@ -4,4 +4,4 @@ docker-compose run build-env bash -c '(cd casper-node; make build-client-contrac
 
 export PUBKEYHEX=$(cat etc/casper/validator_keys/public_key_hex)
 
-docker-compose exec casper-node casper-client put-deploy --chain-name casper-delta-1 --node-address http://3.137.146.20:7777 --secret-key /etc/casper/validator_keys/secret_key.pem --session-path  /contracts/release/add_bid.wasm  --payment-amount 10000000000  --session-arg=public_key:"public_key='$PUBKEYHEX'" --session-arg=amount:"u512='1000009'" --session-arg=delegation_rate:"u64='10'"
+docker-compose exec casper-node casper-client put-deploy --chain-name casper-delta-1 --node-address http://18.144.69.216:7777/ --secret-key /etc/casper/validator_keys/secret_key.pem --session-path  /contracts/release/add_bid.wasm  --payment-amount 10000000000  --session-arg=public_key:"public_key='$PUBKEYHEX'" --session-arg=amount:"u512='1000009'" --session-arg=delegation_rate:"u64='10'"
