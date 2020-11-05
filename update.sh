a423d1f7f4ca4c4100f3d489c76c764a26716d10
Sebastian Heyden <379651+czarly@users.noreply.github.com>
2020-11-06 00:44:35 +0400

    added new trusted hash

--- update.sh
+++ update.sh
@@ -6,6 +6,6 @@ git pull
 echo "[network] 
 public_address='$(dig @ns1-1.akamaitech.net ANY whoami.akamai.net +short):34553'" | crudini --merge etc/casper/config.toml
 echo "[node] 
-#trusted_hash='01b86c7851bb9bbb2c5e160ed731561752093e8817a0b092c5ebc5002be3b8d4'" | crudini --merge etc/casper/config.toml
+trusted_hash='122308bc6b6a3630038ba51dca9d7326bdd49989d95af9347d175db10e8f3399'" | crudini --merge etc/casper/config.toml
 
 docker-compose up -d --build --force-recreate
