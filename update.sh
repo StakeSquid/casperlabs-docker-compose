d34b4fc35ac9e0bf54cddc1b6f8fa8ef749775e1
akme <akme@users.noreply.github.com>
2020-11-21 01:26:47 +0300

    Fix update.sh with new port

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ git pull
 TRUSTED_HASH=$(curl -s http://54.177.84.9:7777/status | jq -r '.last_added_block_info | .hash')
 
 echo "[network] 
-public_address='$(dig @ns1-1.akamaitech.net ANY whoami.akamai.net +short):34553'" | crudini --merge etc/casper/config.toml
+public_address='$(dig @ns1-1.akamaitech.net ANY whoami.akamai.net +short):35000'" | crudini --merge etc/casper/config.toml
 echo "[node] 
 trusted_hash='${TRUSTED_HASH:-null}'" | crudini --merge etc/casper/config.toml
 
