42417dd360bb08f1374609972b26e32647d6edbc
Your Name <you@example.com>
2020-11-21 12:19:00 +0100

    update update script

--- update.sh
+++ update.sh
@@ -3,11 +3,18 @@
 git checkout etc/casper/config.toml
 git pull
 
-TRUSTED_HASH=$(curl -s http://54.177.84.9:7777/status | jq -r '.last_added_block_info | .hash')
+TRUSTED_NODE=54.67.67.33
+TRUSTED_HASH=$(curl -s http://$TRUSTED_NODE:7777/status | jq -r '.last_added_block_info | .hash')
 
-echo "[network] 
-public_address='$(dig @ns1-1.akamaitech.net ANY whoami.akamai.net +short):35000'" | crudini --merge etc/casper/config.toml
-echo "[node] 
-trusted_hash='${TRUSTED_HASH:-null}'" | crudini --merge etc/casper/config.toml
+echo "trusted hash is $TRUSTED_HASH\n"
+
+crudini --set etc/casper/config.toml "network" "public_address" "'$(dig @ns1-1.akamaitech.net ANY whoami.akamai.net +short):35000'"
+
+if [ -z "$TRUSTED_HASH" ]
+then
+	crudini --del etc/casper/config.toml "node" "trusted_hash"
+else
+	crudini --set etc/casper/config.toml "node" "trusted_hash" "'${TRUSTED_HASH}'"    
+fi
 
 docker-compose up -d --build --force-recreate
